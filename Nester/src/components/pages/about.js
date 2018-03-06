d184865202bf24d9df5d208724060933165269cd
Tinnitus95 <31408430+Tinnitus95@users.noreply.github.com>
2018-03-06 15:10:06 +0100

    fix about page

--- Nester/src/components/pages/about.js
+++ Nester/src/components/pages/about.js
@@ -10,10 +10,11 @@ class About extends Component {
                     <div className="row">
                         <div className="col-lg-8 mx-auto">
                             <h2 className="white_me">About NEST:R</h2>
-                            <p>NEST:R is a geolocation game where you compete over the control of Points of Interest on the map.</p>
-                            <p>The game idea is simple, all you have to do is to capture as many nests as possible.<br />
-                            Are you ready to claim what's yours and expand your territory?<br />
+                            <p className="about-text-main">NEST:R is a geolocation game where you compete over the control of Points of Interest on the map.</p>
+                            <p className="about-text">The game idea is simple, all you have to do is to capture as many nests as possible.<br />
+                            Are you ready to claim what's yours and expand your territory?
                             Sign up now and play for FREE!</p>
+                            <img src={screen}></img>
                         </div>
                     </div>
                 </div>
